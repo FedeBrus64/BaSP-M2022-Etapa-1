76f60e1326881abeed00469d5d4e4c9fe883a603
Federico Bruschi <fedeJbruschi@gmail.com>
2022-04-19 16:45:37 -0300

    BaSP-2022: Exercise 01 corrected

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -1,22 +1,31 @@
+console.log('01: VARIABLES AND OPERATORS');
+console.log('----------------------------------------');
+
 // a. Crear dos variables numéricas y utilizar el operador suma para guardar 
 // el valor de la suma de ambos números en una 3er variable.
 var number1 = 4;
 var number2 = 8;
-var resul = number1 + number2;
+var result = number1 + number2;
 
-console.log('Resultado: ', resul);
+console.log('Exercise 1.a');
+console.log('Result: ', result);
 
 // b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
-var str1 = 'mis';
-var str2 = 'perros';
-var str3 = str1.concat(' ',str2);
+var string1 = 'mis';
+var string2 = 'perros';
+var string3 = string1.concat(' ',string2);
 
-console.log(str3);
+console.log('Exercise 1.b');
+console.log(string3);
 
 // c. Crear dos variables de tipo String y sumar el largo de cada variable 
 // (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
-var str1 = 'mis';
-var str2 = 'perros';
-var largo = str1.length + str2.length;
+var string1 = 'mis';
+var string2 = 'perros';
+var totalLength = string1.length + string2.length;
 
-console.log("Cantidad total de letras: ", largo);
+console.log('Exercise 1.c');
+console.log("Total number of letters: ", totalLength);
+
+
+console.log('----------------------------------------');
